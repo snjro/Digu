225fc7f32c9f91f973e4461bd76cc45577b07ca4
snjro <snmail1223@gmail.com>
2023-07-14 16:01:17 +0900

    Fix: consider contract doesn't emit an event

--- src/routes/[chainName]/[projectName_versionName]/contracts/GridCellSyncStatusCurrentState.svelte
+++ src/routes/[chainName]/[projectName_versionName]/contracts/GridCellSyncStatusCurrentState.svelte
@@ -8,6 +8,8 @@
   import { sizeSettings } from "$lib/appearanceConfig/size/sizeSettings";
   import { colorSettings } from "$lib/appearanceConfig/color/colorSettings";
   import CommonSyncCurrentState from "$lib/common/CommonSyncCurrentState.svelte";
+  import BaseLabel from "$lib/base/BaseLabel.svelte";
+  import { NO_DATA } from "@utils/utilsCostants";
 
   export let targetChain: Chain;
   export let targetProject: Project;
@@ -15,11 +17,15 @@
   export let targetContract: Contract;
 </script>
 
-<CommonSyncCurrentState
-  {targetChain}
-  {targetProject}
-  {targetVersion}
-  {targetContract}
-  size={sizeSettings.grid}
-  colorCategoryFront={colorSettings.gridCell}
-/>
+{#if targetContract.events.abiFragments.length > 0}
+  <CommonSyncCurrentState
+    {targetChain}
+    {targetProject}
+    {targetVersion}
+    {targetContract}
+    size={sizeSettings.grid}
+    colorCategoryFront={colorSettings.gridCell}
+  />
+{:else}
+  <BaseLabel text={NO_DATA} textSize={sizeSettings.grid} />
+{/if}
