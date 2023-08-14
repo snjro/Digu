476269ed533ab5ba487a105259365602ae7ad0d8
snjro <snmail1223@gmail.com>
2023-08-14 09:36:56 +0900

    Feat: add `BaseDividerHorizontal.svelte`

---
+++ src/lib/base/BaseDividerHorizontal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {
+    colorDefinitions,
+    type ColorCategory,
+  } from "$lib/appearanceConfig/color/colorDefinitions";
+  import classNames from "classnames";
+  import type { ThemeColor } from "@db/dbTypes";
+  import { storeUserSettings } from "@stores/storeUserSettings";
+
+  export let colorCategory: ColorCategory;
+  export let hidden: boolean = false;
+  let themeColor: ThemeColor;
+  $: themeColor = $storeUserSettings.themeColor as ThemeColor;
+</script>
+
+<div
+  {hidden}
+  class={classNames(
+    "h-[1px]",
+    "w-full",
+    "border-t",
+    colorDefinitions[themeColor][colorCategory].border
+  )}
+/>
