fcdeee45d9b9be0f9a33033ea30fd22528a9b6fa
snjro <snmail1223@gmail.com>
2023-08-14 10:33:36 +0900

    Style: remove some popup effects for dark mode

--- src/lib/base/BaseButton.svelte
+++ src/lib/base/BaseButton.svelte
@@ -186,12 +186,12 @@
       if (hoverEffect && !disabled) {
         if (themeColor === "dark") {
           popup = classNames(
-            "transition",
-            "ease-in-out",
+            // "transition",
+            // "ease-in-out",
             // "delay-150",
             // "hover:-translate-y-1",
-            "hover:scale-105",
-            "duration-150",
+            // "hover:scale-105",
+            // "duration-150",
             "active:top-px"
           );
         } else {
