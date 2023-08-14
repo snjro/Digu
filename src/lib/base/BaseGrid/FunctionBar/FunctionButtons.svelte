0cac3b07a7caf0187010f908e4cf645f1eb15195
snjro <snmail1223@gmail.com>
2023-08-14 14:46:19 +0900

    Refactor: reordered props  in `buttonDefinitions`

--- src/lib/base/BaseGrid/FunctionBar/FunctionButtons.svelte
+++ src/lib/base/BaseGrid/FunctionBar/FunctionButtons.svelte
@@ -17,76 +17,76 @@
   export let isFullScreen: boolean;
   export let quickSearchText: string;
   export let exportFilePrefix: ExportFilePrefix;
+  let buttonDefinitions: Record<string, SimplifiedButtonDefinition[]>;
   $: buttonDefinitions = {
     groupColumnHandler: [
       {
         iconName: "arrowExpandHorizontal",
         tooltipText: "Show all columns",
-        onClickEventFunction: () =>
-          setAllColumnGroupState(gridOptions.columnApi!, true),
         tooltipXPosition: isFullScreen ? "left" : "right",
         tooltipYPosition: isFullScreen ? "bottom" : "top",
+        onClickEventFunction: () =>
+          setAllColumnGroupState(gridOptions.columnApi!, true),
       } as SimplifiedButtonDefinition,
       {
         iconName: "arrowCollapseHorizontal",
         tooltipText: "Hide minor columns",
-        onClickEventFunction: () =>
-          setAllColumnGroupState(gridOptions.columnApi!, false),
         tooltipXPosition: isFullScreen ? "left" : "right",
         tooltipYPosition: isFullScreen ? "bottom" : "top",
+        onClickEventFunction: () =>
+          setAllColumnGroupState(gridOptions.columnApi!, false),
       } as SimplifiedButtonDefinition,
     ],
     columnWidthHandler: [
       {
         iconName: "fitToPageOutline",
         tooltipText: "Fit columns in frame",
-        onClickEventFunction: () => gridOptions.api!.sizeColumnsToFit(),
         tooltipXPosition: isFullScreen ? "left" : "right",
         tooltipYPosition: isFullScreen ? "bottom" : "top",
+        onClickEventFunction: () => gridOptions.api!.sizeColumnsToFit(),
       } as SimplifiedButtonDefinition,
       {
         iconName: "tableColumnWidth",
         tooltipText: "Auto fit columns",
-        onClickEventFunction: () => setAutoColumnWidth(gridOptions.columnApi!),
         tooltipXPosition: isFullScreen ? "left" : "right",
         tooltipYPosition: isFullScreen ? "bottom" : "top",
+        onClickEventFunction: () => setAutoColumnWidth(gridOptions.columnApi!),
       } as SimplifiedButtonDefinition,
     ],
     reset: [
       {
         iconName: "filterRemove",
         tooltipText: "Reset all filters",
-        onClickEventFunction: resetAllFilters,
         tooltipXPosition: isFullScreen ? "left" : "right",
         tooltipYPosition: isFullScreen ? "bottom" : "top",
+        onClickEventFunction: resetAllFilters,
       } as SimplifiedButtonDefinition,
       {
         iconName: "refresh",
         tooltipText: "Reload",
-        onClickEventFunction: reload,
         tooltipXPosition: isFullScreen ? "left" : "right",
         tooltipYPosition: isFullScreen ? "bottom" : "top",
+        onClickEventFunction: reload,
       } as SimplifiedButtonDefinition,
       {
         iconName: "download",
         tooltipText: "Export as CSV",
+        tooltipXPosition: "left",
+        tooltipYPosition: isFullScreen ? "bottom" : "top",
         onClickEventFunction: () => {
           openDialogExportCsv(dialogElement);
         },
-
-        tooltipXPosition: "left",
-        tooltipYPosition: isFullScreen ? "bottom" : "top",
       } as SimplifiedButtonDefinition,
     ],
     fullScreen: [
       {
         iconName: isFullScreen ? "fullScreenExit" : "fullScreen",
         tooltipText: isFullScreen ? "Exit full screen" : "Full screen",
+        tooltipXPosition: "left",
+        tooltipYPosition: isFullScreen ? "bottom" : "top",
         onClickEventFunction: () => {
           isFullScreen = !isFullScreen;
         },
-        tooltipXPosition: "left",
-        tooltipYPosition: isFullScreen ? "bottom" : "top",
       } as SimplifiedButtonDefinition,
     ],
   };
