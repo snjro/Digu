c45744d483e3cd21961e47f6157544f4af62d3ed
snjro <snmail1223@gmail.com>
2023-08-22 23:33:03 +0900

    fix: make recordCount reactive

--- src/routes/[chainName]/[projectName_versionName]/contracts/[contractName]/events/GridCellNumOfLogs.svelte
+++ src/routes/[chainName]/[projectName_versionName]/contracts/[contractName]/events/GridCellNumOfLogs.svelte
@@ -6,31 +6,34 @@
   import type { ContractIdentifier } from "@db/dbTypes";
   import BaseA from "$lib/base/BaseA.svelte";
   import { numberWithCommas } from "@utils/utilsCommon";
+  import { convertTabValueForHref } from "$lib/base/BasePage/BasePageContainerContent.svelte";
 
   export let contractIdentifier: ContractIdentifier;
   export let targetEventName: AbiFragmentName;
   export let urlPathName: string;
-
-  let recordCount: number;
-  $: recordCount =
+  let currentRecordCount: number = 0;
+  $: currentRecordCount =
     $storeSyncStatus[contractIdentifier.chainName].subSyncStatuses[
       contractIdentifier.projectName
     ].subSyncStatuses[contractIdentifier.versionName].subSyncStatuses[
       contractIdentifier.contractName
     ].events[targetEventName].recordCount;
-  const href: string = `${urlPathName}/${targetEventName}`;
+
+  const href: string = `${urlPathName}/${targetEventName}${convertTabValueForHref(
+    "Event Logs (text)"
+  )}`;
 </script>
 
-{#if recordCount > 0}
+{#if currentRecordCount > 0}
   <BaseA
-    text={numberWithCommas(recordCount)}
+    text={numberWithCommas(currentRecordCount)}
     {href}
     textSize={sizeSettings.grid}
     openNewTab={false}
   />
 {:else}
   <BaseLabel
-    text={numberWithCommas(recordCount)}
+    text={numberWithCommas(currentRecordCount)}
     textSize={sizeSettings.grid}
   />
 {/if}
