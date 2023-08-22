f7efede4dc5bf2a20932c72112c52dc244a778c2
snjro <snmail1223@gmail.com>
2023-08-23 08:47:05 +0900

    refactor: set props as variables

--- src/routes/[chainName]/[projectName_versionName]/contracts/[contractName]/events/GridCellNumOfLogs.svelte
+++ src/routes/[chainName]/[projectName_versionName]/contracts/[contractName]/events/GridCellNumOfLogs.svelte
@@ -7,10 +7,12 @@
   import BaseA from "$lib/base/BaseA.svelte";
   import { numberWithCommas } from "@utils/utilsCommon";
   import { convertTabValueForHref } from "$lib/base/BasePage/BasePageContainerContent.svelte";
+  import type { BaseSize } from "$lib/base/baseSizes";
 
   export let contractIdentifier: ContractIdentifier;
   export let targetEventName: AbiFragmentName;
   export let urlPathName: string;
+
   let currentRecordCount: number = 0;
   $: currentRecordCount =
     $storeSyncStatus[contractIdentifier.chainName].subSyncStatuses[
@@ -22,18 +24,15 @@
   const href: string = `${urlPathName}/${targetEventName}${convertTabValueForHref(
     "Event Logs (text)"
   )}`;
+
+  let text: string;
+  $: text = numberWithCommas(currentRecordCount);
+
+  const textSize: BaseSize = sizeSettings.grid;
 </script>
 
 {#if currentRecordCount > 0}
-  <BaseA
-    text={numberWithCommas(currentRecordCount)}
-    {href}
-    textSize={sizeSettings.grid}
-    openNewTab={false}
-  />
+  <BaseA {text} {href} {textSize} openNewTab={false} />
 {:else}
-  <BaseLabel
-    text={numberWithCommas(currentRecordCount)}
-    textSize={sizeSettings.grid}
-  />
+  <BaseLabel {text} {textSize} />
 {/if}
