d5e3b6c939efef78dbeaa50c0469d5561900023b
snjro <snmail1223@gmail.com>
2023-09-04 12:26:53 +0900

    refactor: add event `clickOutside`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,6 @@
   import { breakPointWidths } from "$lib/appearanceConfig/size/sizeDefinitions";
   import BaseSpinner from "$lib/base/BaseSpinner.svelte";
   import LeftSidebar from "$lib/leftSidebar/LeftSidebar.svelte";
-  import { toggleLeftSideBar } from "$lib/leftSidebar/functions";
   import type { ThemeColor } from "@db/dbTypes";
   import { storeNoDbCurrentWidth } from "@stores/storeNoDb";
   import { storeUserSettings } from "@stores/storeUserSettings";
@@ -30,7 +29,6 @@
   $: themeColor = $storeUserSettings.themeColor as ThemeColor;
 
   $: bodyStyle = classNames(
-    "relative",
     "flex",
     "flex-row",
     "h-screen",
@@ -42,24 +40,15 @@
 
   $: mainContainerStyle = classNames(
     classNames(
+      "min-w-0 flex-auto",
       "flex",
       "flex-col",
-      "w-screen",
-      "h-screen",
       $storeUserSettings.isOpenSidebar &&
-        $storeNoDbCurrentWidth > breakPointWidths.sm &&
-        "leftsidebar-padding",
-      "cursor-default",
+        $storeNoDbCurrentWidth <= breakPointWidths.sm &&
+        "blur-sm pointer-events-none",
     ),
   );
-  $: showFilter =
-    $storeUserSettings.isOpenSidebar &&
-    $storeNoDbCurrentWidth <= breakPointWidths.sm;
-  const clickFilter = async (): Promise<void> => {
-    if (showFilter) {
-      await toggleLeftSideBar();
-    }
-  };
+
   function onResize(): void {
     storeNoDbCurrentWidth.set(getScreenWidth());
   }
@@ -75,27 +64,7 @@
   {:else}
     <LeftSidebar />
     <div class={mainContainerStyle}>
-      <button
-        on:click={clickFilter}
-        class={classNames(
-          "absolute",
-          "h-full",
-          "w-full",
-          !showFilter && "hidden",
-          colorDefinitions[themeColor][colorSettings.leftSidebarHeader].bg,
-          "opacity-60",
-          "blur-sm",
-          "cursor-default",
-        )}
-      />
       <Content><slot /></Content>
     </div>
   {/if}
 </div>
-
-<style lang="scss">
-  @use "../lib/leftSidebar/leftsidebar.scss" as lsb;
-  .leftsidebar-padding {
-    padding-left: lsb.$leftsidebar-width * 1px;
-  }
-</style>
