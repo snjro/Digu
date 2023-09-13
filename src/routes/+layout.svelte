1890d48fe231e9c28c52258733356b15b6809c4a
snjro <snmail1223@gmail.com>
2023-09-13 22:08:15 +0900

    style: add `startViewTransition`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { browser } from "$app/environment";
+  import { onNavigate } from "$app/navigation";
   import { colorDefinitions } from "$lib/appearanceConfig/color/colorDefinitions";
   import { colorSettings } from "$lib/appearanceConfig/color/colorSettings";
   import { breakPointWidths } from "$lib/appearanceConfig/size/sizeDefinitions";
@@ -10,6 +11,7 @@
   import type { ThemeColor } from "@db/dbTypes";
   import { storeNoDbCurrentWidth } from "@stores/storeNoDb";
   import { storeUserSettings } from "@stores/storeUserSettings";
+  import type { OnNavigate } from "@sveltejs/kit";
   import { getScreenWidth } from "@utils/utilsDom";
   import classNames from "classnames";
   import type { LoadDataRoot } from "./+layout";
@@ -32,6 +34,17 @@
   function onResize(): void {
     storeNoDbCurrentWidth.set(getScreenWidth());
   }
+
+  onNavigate((navigation: OnNavigate) => {
+    if (!(document as any).startViewTransition) return;
+
+    return new Promise((resolve) => {
+      (document as any).startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 
 <svelte:window on:resize={onResize} />
