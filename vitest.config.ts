f2ad4b42f344bc490cd9b5d9ea186d16b3ae4aea
snjro <snmail1223@gmail.com>
2023-09-13 16:50:27 +0900

    test: add packages for test

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+  plugins: [svelte({ hot: !process.env.VITEST })],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    coverage: {
+      provider: "v8",
+    },
+  },
+});
