4fb1f44f8c9f188eb0d7cdf6fcfeb407fb33f7c7
snjro <snmail1223@gmail.com>
2023-09-28 13:15:27 +0900

    test: add mocked class

--- src/utils/utilsEthers.test.ts
+++ src/utils/utilsEthers.test.ts
@@ -68,6 +68,8 @@ describe("getNodeProvider", async () => {
   let spyJsonRpcGetNetwork: any;
   let spyWebSocketGetNetWork: any;
   beforeAll(() => {
+    vi.mock("JsonRpcProvider");
+    vi.mock("WebSocketProvider");
     spyJsonRpcGetNetwork = vi
       .spyOn(JsonRpcProvider.prototype, "getNetwork")
       .mockImplementationOnce(() => {
