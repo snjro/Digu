c9c1ebc04773605b47ba25bea54f9e85fbbd7971
snjro <snmail1223@gmail.com>
2023-10-10 16:45:44 +0900

    initial

---
+++ _deploy/_app/immutable/chunks/columnDefStateMutability.8a731fd6.js
@@ -0,0 +1,1 @@
+import{d as t}from"./BaseButton.411e578b.js";const o=e=>({field:e,headerName:"State Mutability",sortable:!0,editable:!1,cellClass:t("text-center"),columnGroupShow:void 0});export{o as c};
