b7c2fe642c18be5d5454bad178ab8167d64c1c47
snjro <snmail1223@gmail.com>
2023-10-11 11:15:50 +0900

    fix: replace method "goto" to "redirect"

--- src/routes/page.ts.test.ts
+++ src/routes/page.ts.test.ts
@@ -13,6 +13,9 @@ import { load } from "./+page";
 import * as appNav from "$app/navigation";
 import { goto } from "$app/navigation";
 
+import * as kit from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
+
 type BrowserValue = { browserValue: boolean };
 const browserValues: BrowserValue[] = [
   { browserValue: true },
@@ -37,32 +40,36 @@ describe("load", () => {
     .spyOn(DbSettingsDataHandlers, "getDbItemUserSettings")
     .mockResolvedValue(expectedSlelectedChainName);
 
+  const spyRedirect = vi.spyOn(kit, "redirect");
+
   test.each(browserValues)(`should $browserValue`, async ({ browserValue }) => {
     mockBrowser = browserValue;
     beforeEach(() => {
       // clear call count
       spyGetDbItemUserSettings.mockClear();
-
-      // To get a mocked function, cast "goto" as MockedFunction.
-      // https://stackoverflow.com/questions/52457575/jest-typescript-property-mock-does-not-exist-on-type
-      (appNav.goto as MockedFunction<typeof appNav.goto>).mockClear();
     });
     afterAll(() => {
       vi.restoreAllMocks();
     });
-    await load();
+
+    // "redirect" throws, so need to catch.
+    try {
+      await load();
+    } catch (error) {}
+
     if (browserValue) {
       expect(spyGetDbItemUserSettings).toHaveBeenCalledWith(
         "userSetting01",
         "selectedChainName",
       );
+
       expect(spyGetDbItemUserSettings).toHaveReturnedWith(
         expectedSlelectedChainName,
       );
-      expect(goto).toHaveBeenCalledWith(`/${expectedSlelectedChainName}`);
+      expect(spyRedirect).toThrow();
     } else {
       expect(spyGetDbItemUserSettings).not.toBeCalled();
-      expect(goto).not.toHaveBeenCalled();
+      expect(redirect).toThrowError();
     }
   });
 });
