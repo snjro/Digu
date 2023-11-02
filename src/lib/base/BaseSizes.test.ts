048f739ed47aa711301b65d0b8561840fe9445e7
snjro <snmail1223@gmail.com>
2023-11-02 17:29:57 +0900

    test: refactor

--- src/lib/base/BaseSizes.test.ts
+++ src/lib/base/BaseSizes.test.ts
@@ -1,23 +1,34 @@
 import { describe, expect, test } from "vitest";
 import { changeSize, type BaseSize } from "./baseSizes";
 
+type TestParam = {
+  baseSize: BaseSize;
+  notch: number;
+  expectedSize: BaseSize;
+  text: string;
+};
+const testParams: TestParam[] = [
+  {
+    baseSize: "4xl",
+    notch: 2,
+    expectedSize: "5xl",
+    text: "should return the max size when changed size exceeds upper limit",
+  },
+  {
+    baseSize: "sm",
+    notch: -2,
+    expectedSize: "xs",
+    text: "should return the min size when changed size exceeds lower limit",
+  },
+  {
+    baseSize: "md",
+    notch: 1,
+    expectedSize: "lg",
+    text: "should return the change size when changed size is int the range",
+  },
+];
 describe("changeSize", () => {
-  test("should return the max size when changed size exceeds upper limit", () => {
-    const baseSize: BaseSize = "4xl";
-    const notch: number = 2;
-    const expectedSize: BaseSize = "5xl";
-    expect(changeSize(baseSize, notch)).toBe(expectedSize);
-  });
-  test("should return the min size when changed size exceeds lower limit", () => {
-    const baseSize: BaseSize = "sm";
-    const notch: number = -2;
-    const expectedSize: BaseSize = "xs";
-    expect(changeSize(baseSize, notch)).toBe(expectedSize);
-  });
-  test("should return the change size when changed size is int the range", () => {
-    const baseSize: BaseSize = "md";
-    const notch: number = 1;
-    const expectedSize: BaseSize = "lg";
+  test.each(testParams)("$text", ({ baseSize, notch, expectedSize }) => {
     expect(changeSize(baseSize, notch)).toBe(expectedSize);
   });
 });
