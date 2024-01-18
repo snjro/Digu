c5e0293329298ba18350410d7c9951b7d47c4aab
snjro <snmail1223@gmail.com>
2024-01-18 17:27:59 +0900

    fix: add `mockRestore` after each test

--- src/db/dbEventLogsGetUpdateTargetEventLogTables.test.ts
+++ src/db/dbEventLogsGetUpdateTargetEventLogTables.test.ts
@@ -1,5 +1,12 @@
 import "fake-indexeddb/auto";
-import { describe, expect, test, vi } from "vitest";
+import {
+  afterEach,
+  describe,
+  expect,
+  test,
+  vi,
+  type MockInstance,
+} from "vitest";
 import * as UtilsDb from "@utils/utlisDb";
 import { DbEventLogs } from "./dbEventLogs";
 import type { GroupedEventLogs, VersionIdentifier } from "./dbTypes";
@@ -10,32 +17,35 @@ import type { Table } from "dexie";
 import { DB_TABLE_NAMES } from "./constants";
 
 describe("getUpdateTargetEventLogTables", () => {
-  test("", () => {
-    for (const targetChain of TARGET_CHAINS) {
-      for (const targetProject of targetChain.projects) {
-        for (const targetVersion of targetProject.versions) {
-          const versionIdentifier: VersionIdentifier = {
-            chainName: targetChain.name,
-            projectName: targetProject.name,
-            versionName: targetVersion.name,
-          };
-          const dbEventLogs: DbEventLogs = new DbEventLogs(versionIdentifier);
-          for (const targetContract of targetVersion.contracts) {
+  for (const targetChain of TARGET_CHAINS) {
+    for (const targetProject of targetChain.projects) {
+      for (const targetVersion of targetProject.versions) {
+        const versionIdentifier: VersionIdentifier = {
+          chainName: targetChain.name,
+          projectName: targetProject.name,
+          versionName: targetVersion.name,
+        };
+        const dbEventLogs: DbEventLogs = new DbEventLogs(versionIdentifier);
+        for (const targetContract of targetVersion.contracts) {
+          // set spy
+          let spyGetEventLogTableName: MockInstance;
+          // restore
+          afterEach(() => {
+            if (spyGetEventLogTableName) {
+              spyGetEventLogTableName.mockRestore();
+            }
+          });
+          test("should return eventLog tables and syncStatus table when groupedEventLogs is NOT empty", () => {
             const groupedEventLogs: GroupedEventLogs = getDummyGroupedEventLogs(
               targetContract.events.names,
             );
-            // set spy
-            const spyGetEventLogTableName = vi.spyOn(
-              UtilsDb,
-              "getEventLogTableName",
-            );
+            spyGetEventLogTableName = vi.spyOn(UtilsDb, "getEventLogTableName");
             // call target
             const actualReturnedTables: Table[] = getUpdateTargetEventLogTables(
               dbEventLogs,
               targetContract.name,
               groupedEventLogs,
             );
-
             // expected return
             const expectedReturnTables: Table[] =
               targetContract.events.names.map((eventName) => {
@@ -58,13 +68,11 @@ describe("getUpdateTargetEventLogTables", () => {
             for (let i = 0; i < actualReturnedTables.length; i++) {
               expect(actualReturnedTables[i]).toEqual(expectedReturnTables[i]);
             }
-            // restore
-            spyGetEventLogTableName.mockRestore();
-          }
+          });
         }
       }
     }
-  });
+  }
 });
 function getDummyGroupedEventLogs(
   eventNames: EventAbiFragment["name"][],
