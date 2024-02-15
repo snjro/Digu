f5660c203476a4f3a093982150fb4e510debd9e5
snjro <snmail1223@gmail.com>
2024-02-15 15:36:08 +0900

    test: add false pattern

--- src/utils/utilsType.test.ts
+++ src/utils/utilsType.test.ts
@@ -1,14 +1,20 @@
 import { describe, expect, test } from "vitest";
+type TestType = {
+  a: number;
+  b: string;
+  c: boolean;
+};
 
 describe("KeysMatching", () => {
-  test("should return a key of an object that match a specific type", () => {
-    type TestObject = {
-      a: string;
-      b: number;
-      c: boolean;
-    };
-    type ExpectedResult = KeysMatching<TestObject, string>;
+  test("should return keys of matching types", () => {
+    type ExpectedResult = KeysMatching<TestType, number>;
     const expectedResult: ExpectedResult = "a";
     expect(expectedResult).toBe("a");
+  });
+  test("should not include keys of non-matching types", () => {
+    type ExpectedResult = KeysMatching<TestType, number>;
+    const keys: ExpectedResult[] = ["a"];
+    expect(keys).not.toContain("b");
+    expect(keys).not.toContain("c");
   });
 });
