844d18c5064ca8b7a2a36aedd7d7dcb8b3112887
snjro <34432943+snjro@users.noreply.github.com>
2026-09-23 17:34:05 +0900

    chore: remove unused Mumbai chain data

--- src/constants/chains/mumbai/augur/_index.ts
+++
@@ -1,7 +0,0 @@
-import type { Project } from "@constants/chains/types";
-import { version as turbo } from "./turbo/_index";
-
-export const project: Project = {
-  name: "Augur",
-  versions: [turbo],
-};
