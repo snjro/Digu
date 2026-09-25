97f1eb8a386e7efa5bab7d159ac859cb2b679df1
snjro <34432943+snjro@users.noreply.github.com>
2026-09-26 08:32:50 +0900

    test: bind a $state tab definition to PageWrapper and select the tab of the URL hash

---
+++ src/lib/PageWrapper/PageWrapper.testHost.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import PageWrapper from "./PageWrapper.svelte";
+  import { TAB_VALUES_CONTRACT, type TabsDefinitionContract } from "./tabs";
+
+  let tabsDefinition: TabsDefinitionContract = $state({
+    selected: "Overview",
+    values: TAB_VALUES_CONTRACT,
+    groupName: "tabGroupContract",
+  });
+</script>
+
+<PageWrapper bind:tabsDefinition />
+<span data-testid="selected">{tabsDefinition.selected}</span>
